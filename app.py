8e18feb67380785a72f6a802be4d0960573b6a2c
Akash Das Argula <aad06arg@gmail.com>
2026-01-08 17:06:15 +0000

    website almost done

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 45178,
+    'title': 'Python Developer',
+    'location': 'San Francisco, CA',
+    'Salary': '$150,000'
+  },
+  {
+    'id': 78341,
+    'title': 'UI Designer',
+    'location': 'Cupertino, CA',
+    'Salary': '$130,000'
+  },
+  {
+  'id': 65937,
+  'title': 'Database Administrator',
+  'location': 'Manhattan, NY',
+  'Salary': '$110,000'
+  }
+]
+
 @app.route('/')
 def website_1():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, )
   
 
 if __name__ == '__main__':
