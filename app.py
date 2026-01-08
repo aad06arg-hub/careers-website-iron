2a12aaf8a4e634e48f681909d93fa97c96862a5e
Akash Das Argula <aad06arg@gmail.com>
2026-01-08 15:47:42 +0000

    added CSS using external framework Bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def website_1():
-  return "<h1>This is website 1 view</h1>"
+  return render_template('home.html')
   
 
 if __name__ == '__main__':
